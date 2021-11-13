278f48e53f539c7a4c6483a0f45d9f4101e41617
andrew bumgarner <andrewbumgarnerNC@gmail.com>
2021-11-13 12:56:36 -0800

    completed login/logout/create user routes create user will automatically sign in user after sign up

--- routes/api/trash-routes.js
+++ routes/api/trash-routes.js
@@ -1,4 +1,5 @@
 //this is using https
+//Alicia you're doing a great job! 
 function edamamv2() {https.get('https://api.edamam.com/api/food-database/v2/parser?app_id=64a0e39a&app_key=90e521d5fd7b9f2ee89888aaa573e898&ingr=mexican&nutrition-type=cooking&category=fast-foods&calories=500', (resp) => {
   let data = '';
   let parsedData;
