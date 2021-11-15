fa3bb79d2c62621b02a46f58fd1476506d914c70
alicia chen <aliciachen10@gmail.com>
2021-11-15 15:26:56 -0500

    get homepage displaying

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -9,6 +9,28 @@ const bcrypt = require('bcrypt');
 //get all exercises, with their exercises and preferences
 router.get('/', async (req, res) => {
   try {
+    // const exerciseData = await Exercise.findAll({
+    //   include: [{ model: User }, { model: Activity }],
+    //   where: { 
+    //     user_id: req.session.user_id 
+    //   },
+    // });
+    // res.status(200).json(exerciseData);
+    // const exercises = exerciseData.map((exercise) => exercise.get({ plain: true })).reverse();
+    res.render('homepage', { 
+      loggedIn: req.session.loggedIn,
+      // user_id: req.session.user_id
+     })
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
+});
+
+//render user's exercise history
+//get all exercises, with their exercises and preferences
+router.get('/app', async (req, res) => {
+  try {
     const exerciseData = await Exercise.findAll({
       include: [{ model: User }, { model: Activity }],
       where: { 
