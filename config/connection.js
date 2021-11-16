820f447da9827aef568f3527092e43a38b5613c2
alicia chen <aliciachen10@gmail.com>
2021-11-16 14:04:41 -0500

    Merge branch 'main' into andrew-branch

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 
 let sequelize;
 
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -18,7 +18,7 @@ let sequelize;
       logging: false
     }
   );
-// }
+}
 
 module.exports = sequelize;
 
